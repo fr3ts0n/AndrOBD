dd0befdacfc4dbc2bf1b00766e168e58ac3f5fc9
Erwin Scheuch-Heilig <fr3ts0n@gmx.at>
2026-02-22 12:31:28 +0100

    Fixed BT/BLE permission handling
    - Request BLUETOOTH_SCAN + BLUETOOTH_CONNECT for BLE + classic
    - V2.7.2

--- androbd/src/main/java/com/fr3ts0n/ecu/gui/androbd/MainActivity.java
+++ androbd/src/main/java/com/fr3ts0n/ecu/gui/androbd/MainActivity.java
@@ -634,12 +634,11 @@ public class MainActivity extends PluginManager
         switch (CommService.medium)
         {
             case BLE:
+            case BLUETOOTH:
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
                     log.warning("permission.BLUETOOTH_SCAN missing");
                     ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_SCAN}, 1);
                 }
-                // NO break here
-            case BLUETOOTH:
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
                     log.warning("permission.BLUETOOTH_CONNECT missing");
                     ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1);
